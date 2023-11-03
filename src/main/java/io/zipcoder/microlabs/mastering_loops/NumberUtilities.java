87d9cbb0f873c273b567c93d213886bc643b55bd
Tevin Glover <tevwork.tg@gmail.com>
2023-11-02 22:04:43 -0400

    kash is the best

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -2,13 +2,55 @@ package io.zipcoder.microlabs.mastering_loops;
 
 
 public class NumberUtilities {
+
+    static boolean isOdd( int number){ // created a odd method to call  to use for Q1, Q2
+        return (number % 2) != 0 ;}
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i< stop; i++) {
+            if (isOdd(i)) {
+                sb.append(i);
+                System.out.println(i + " was append to the string");
+                System.out.println(isOdd(i));
+            }// the name of the method is misleading.
+            else {
+                System.out.println(i + " was not add");
+
+            }
+        }
+        return sb.toString();// append turns to a String for us.
+
+
+
+
+
+
+
+       // return null;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = start; i< stop; i++) {
+            if (!isOdd(i)) {
+                sb.append(i);
+                System.out.println(i + " was append to the string");
+                System.out.println(isOdd(i));
+            }// the name of the method is misleading.
+            else {
+                System.out.println(i + " was not add");
+
+            }
+        }
+        return sb.toString();// append turns to a String for us.
+
+
+
+       // return null;
     }
 
 
